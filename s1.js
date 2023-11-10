ac1178b16419005ec43a17255964ee8a06645db5
Krutika44 <shirsatkrutika4@gmail.com>
2023-11-10 20:35:13 +0530

    commit

---
+++ s1.js
@@ -0,0 +1,5 @@
+let can = document.getElementById("table")
+let draw_ = can.getContext('2d')   //getContext is used to draw anything on canvas
+
+draw_.fillStyle = "black";
+draw_.fillRect(0,0,can.width,can.height);
